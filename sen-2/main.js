348acba09bb248c7e7672293957b34bd4c58397b
Christopher <34822892+christophergouveia@users.noreply.github.com>
2024-03-18 11:37:10 -0300

    Add files via upload

--- sen-2/main.js
+++ sen-2/main.js
@@ -20,9 +20,35 @@ function pesquisarCNPJ(cnpj) {
         return (document.getElementById("dados").innerHTML = `
             ${dados.message}`);
       }
+      var atividadePrincipal = "";
+      dados.atividade_principal.forEach((value) => {
+        atividadePrincipal += `<span>${value.code} - ${value.text}</span>`;
+      });
+
+      var atividadesSecundarias = "";
+      dados.atividades_secundarias.forEach((value) => {
+        atividadesSecundarias += `<span>${value.code} - ${value.text}</span>`;
+      });
+
       document.getElementById("dados").innerHTML = `
     <span>Razão social: ${dados.nome}</span><br>
-    <span>Natureza jurídica: ${dados.natureza_juridica}
+    <span>Natureza jurídica: ${dados.natureza_juridica}</span><br>
+    <span>Nome fatansia: ${dados.fantasia}</span><br>
+    <span>Bairro: ${dados.bairro}</span><br>
+    <span>Porte: ${dados.porte}</span><br>
+    <span>UF: ${dados.uf}</span><br>
+    <span>Email: ${dados.email}</span><br>
+    <span>Telefone: ${dados.telefone}</span><br>
+    <span>CEP: ${dados.cep}</span><br>
+    <span>Logradouro: ${dados.logradouro}</span><br>
+    <div id='atividades'>
+    <h3>Atividades principais: </h3>
+      ${atividadePrincipal}
+      </div>
+    <div id='atividades'>
+    <h3>Atividades Secundarias: </h3>
+      ${atividadesSecundarias}
+      </div>
     `;
     });
 }
